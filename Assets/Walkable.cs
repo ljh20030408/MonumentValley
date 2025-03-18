c2da31dc65cc6ec7edf21986c377c75c1f0490c7
笙酒 <752501297@qq.com>
2025-03-18 17:37:47 +0800

    first

---
+++ Assets/Walkable.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//关联所有的方块
+public class Walkable : MonoBehaviour
+{
+    
+    //创建possiblePaths的list来储存gamepath的变量
+    public List<WalkPath> possiblePaths = new List<WalkPath>();
+
+    [Space]
+
+    public Transform previousBlock;
+
+    [Space]
+ 
+    [Header("Booleans")]
+    public bool isStair = false;
+    public bool movingGround = false;
+    public bool isButton;
+    public bool dontRotate;
+
+    [Space]
+
+    [Header("Offsets")]
+    public float walkPointOffset = .5f;
+    public float stairOffset = .4f;
+    //利用位移变量去确定玩家路径点
+    public Vector3 GetWalkPoint()
+    {
+        float stair = isStair ? stairOffset : 0;
+        return transform.position + transform.up * walkPointOffset - transform.up * stair;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray;
+        float stair = isStair ? .4f : 0;
+        Gizmos.DrawSphere(GetWalkPoint(), .1f);
+
+        if (possiblePaths == null)
+            return;
+
+        foreach (WalkPath p in possiblePaths)
+        {
+            if (p.target == null)
+                return;
+            Gizmos.color = p.active ? Color.black : Color.clear;
+            Gizmos.DrawLine(GetWalkPoint(), p.target.GetComponent<Walkable>().GetWalkPoint());
+        }
+    }
+}
+
+[System.Serializable]
+
+
+public class WalkPath
+{
+    public Transform target;
+    public bool active = true;
+}
